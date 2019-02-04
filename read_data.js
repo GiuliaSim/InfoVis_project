7d7c94082492c9df79a2473c60d294bf4d1e3069
GiuliaSim <giuly.simonelli@gmail.com>
2019-02-04 12:01:06 +0100

    fixed error

--- read_data.js
+++ read_data.js
@@ -52,7 +52,9 @@ d3.text("data/out-communities-SToClustering.txt", function(error, text) {
   	var textsplitted = text.split("\n");
   	textsplitted.map(function(d){
   		var community = d.split(",").map(Number);
-  		communities.push(community);
+  		if(d.length > 0){
+	  		communities.push(community);
+	  	}
   	});
 
   	var psv = d3.dsvFormat(";");
@@ -89,7 +91,7 @@ d3.text("data/out-communities-SToClustering.txt", function(error, text) {
           		nodes: nodes,
           		links: links
           	}
-
+          	//var gggg = nodes.filter(function(d){return typeof d.main_topic == 'undefined';}); 
           	var min = d3.min(nodes, function(d){return d.main_topic;})
           	var max = d3.max(nodes, function(d){return d.main_topic;})
           	console.log("main_topic. min:" + min + " max: " + max);
@@ -127,8 +129,11 @@ d3.text("data/out-communities-SToClustering.txt", function(error, text) {
 		  	//Viene creato l'array di clusters identificati. 
     		clusterSizeDistr.map(function(d, i){clusters[i] = {cluster: i, size: d.size, count: d.communities.length};})
 
-		  	//console.log(communities);
+		  	console.log("communities");
+		  	console.log(communities);
+		  	console.log("communities_attributes");
 		  	console.log(communities_attributes);
+		  	console.log("clusterSizeDistr");
 		  	console.log(clusterSizeDistr);
 		  	//console.log(clusters);
 
@@ -252,7 +257,7 @@ d3.text("data/out-communities-SToClustering.txt", function(error, text) {
  				var min = d3.min(x, function(v){return v.value;});
           		var max = d3.max(x, function(v){return v.value;});
 
-				main_topics_comm.push({ size: d.size, main_topics: x, statistics_common_topics: {avg: avg, max: max, min: min} });
+				main_topics_comm.push({ id: d.id, size: d.size, main_topics: x, statistics_common_topics: {avg: avg, max: max, min: min} });
 			});
 
 			console.log(main_topics_comm);
